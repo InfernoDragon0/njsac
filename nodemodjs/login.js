3c3038d829870e24569b0fab585871b761da4d48
SageSG <houliangyip@gmail.com>
2017-08-10 12:36:47 +0800

    push test

--- nodemodjs/login.js
+++ nodemodjs/login.js
@@ -1,6 +1,7 @@
 var ejs = require('ejs'); //ejs is not express, but is a extension to express
 var path = require('path'); //pathing system
 var bodyParser = require('body-parser'); //parse POST data
+const sql = require('mssql')
 const express = require('express'); //express is good
 const app = express();
 
@@ -13,34 +14,13 @@ var config = {
     server: 'jedb.database.windows.net',
     options: { database: 'testDB', encrypt: true }
 };
-
-var selectusername = connection.query("select * from adminaccount where adminName='"+body.username+"'").toArray((error,results)=>{
-if (error){
-console.log('Error in the query');
-}else{
-    if (results.length < 1) {
-                    console.log('No data found');
-                    resolve('-1');
-                    return;
-                }
-console.log('Successful query');
-    for(let result of results){
-        var accountId = result['accountId'],
-        var adminId = result['adminId'],
-        var adminName = result['adminName'],
-        var adminPassword = result['adminPassword']
-    };
-};
-});
-
-app.post('/main'),function(req,res){
-    if(req.body.username===adminName && req.body.password===adminPassword){
+ 
+async () => {
+    try {
+        const pool = await sql.connect('mssql://accountant:Abcd1234@jedb.databse.windows.net/testDB')
+        const result = await sql.query`select * from adminaccount where accountId = ${value}`
+        console.dir(result)
+    } catch (err) {
+        // ... error checks 
     }
-        else{
-    };
-};
-
-module.exports.checklogin=checklogin;
-function checklogin(){
-
-};
+}
