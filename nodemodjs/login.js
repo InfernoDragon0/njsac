d54279f6815cea7f60c77e764f15a93cb80eb716
SageSG <houliangyip@gmail.com>
2017-08-10 16:13:10 +0800

    body parser

--- nodemodjs/login.js
+++ nodemodjs/login.js
@@ -2,18 +2,27 @@
 var ejs = require('ejs'); //ejs is not express, but is a extension to express
 var path = require('path'); //pathing system
 var bodyParser = require('body-parser'); //parse POST data
-const express = require('express'); //express is good
-const app = express();
+var express = require('express'); //express is good
+var app = express();
 var http = require('http');
+
+app.set('view engine', 'ejs');
+app.use('/njsac', express.static('njsac')
+);
+
+app.get ('/', function(req,res){
+    res.render('index');
+});
+
+app.get('/login', function(req,res){
+    res.render('login');
+});
 
 // create application/x-www-form-urlencoded parser 
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 app.post('/login', urlencodedParser, function(req,res){
-    if (!req.body) return res.sendStatus(400)
-    var name = req.body.username;
-    var pW = req.body.password;
-    console.log(name);
-    console.log(pW);
+    console.log(req.body.username);
+    console.log(req.body.password);
     res.render('login');
 });
